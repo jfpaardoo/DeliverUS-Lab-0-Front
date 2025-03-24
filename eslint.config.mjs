45235848ddf4170196d1dc4d766dbc08b8e9bfea
jfpaardoo <juaparpar@alum.us.es>
2025-03-24 18:26:10 +0100

    Commit inicial

---
+++ eslint.config.mjs
@@ -0,0 +1,34 @@
+import { defineConfig } from "eslint/config";
+import globals from "globals";
+import js from "@eslint/js";
+import pluginReact from "eslint-plugin-react";
+
+export default defineConfig([
+  { files: ["**/*.{js,mjs,cjs,jsx}"] },
+  { files: ["**/*.{js,mjs,cjs,jsx}"], languageOptions: { globals: globals.browser } },
+  { files: ["**/*.{js,mjs,cjs,jsx}"], plugins: { js }, extends: ["js/recommended"] },
+  pluginReact.configs.flat.recommended,
+  {
+    env: {
+      browser: true,
+      es2021: true
+    },
+    extends: [
+      'plugin:react/recommended',
+      'standard'
+    ],
+    parserOptions: {
+      ecmaFeatures: {
+        jsx: true
+      },
+      ecmaVersion: 'latest',
+      sourceType: 'module'
+    },
+    plugins: [
+      'react'
+    ],
+    rules: {
+      'react/prop-types': 'off'
+    }
+  }
+]);
